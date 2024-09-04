7cf8639e9fe10df49fa13bd781ceef242ba8a4c8
GROUD <mark7673@elev.fgukoldingvejen.dk>
2024-09-04 21:46:26 +0200

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,59 @@
+const amount = '?limit=30';
+const API_URL = 'https://pokeapi.co/api/v2/pokemon' + amount;
+
+
+fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
+        const pokemonPromises = data.results.map(pokemon => {
+            return fetch(pokemon.url).then(pokemonResponse => pokemonResponse.json());
+        });
+
+        Promise.all(pokemonPromises).then(pokemonDetails => {
+            const listeDiv = document.getElementById('list');
+            const selectedImageDiv = document.getElementById('image');
+            const selectedInfoDiv = document.getElementById('info');
+
+            pokemonDetails.forEach(pokemon => {
+                const img = document.createElement('img');
+                img.src = pokemon.sprites.front_default;
+
+                img.addEventListener('click', () => {
+                    const newUrl = new URL(window.location);
+                    newUrl.searchParams.set('name', pokemon.name);
+                    window.history.pushState({}, '', newUrl);
+
+                    selectedImageDiv.innerHTML = '';
+                    selectedInfoDiv.innerHTML = '';
+
+                    const selectedImg = document.createElement('img');
+                    selectedImg.src = pokemon.sprites.front_default;
+                    selectedImageDiv.appendChild(selectedImg);
+                    
+                    const info = document.createElement('div');
+                    info.innerHTML = `
+                        <h1>${pokemon.name}</h1>
+                        <p><strong>Base Experience:</strong> ${pokemon.base_experience}</p>
+                        <p><strong>Weight:</strong> ${pokemon.weight}</p>
+                    `;
+                    selectedInfoDiv.appendChild(info);
+
+                    localStorage.setItem('selectedPokemon', JSON.stringify(pokemon));
+                });
+
+                listeDiv.appendChild(img);
+            });
+        });
+    });
+
+
+    document.getElementById('details').addEventListener('click', (event) => {
+        event.preventDefault(); 
+    
+        const selectedPokemon = localStorage.getItem('selectedPokemon');
+        if (selectedPokemon) {
+            window.location.href = 'details.html';
+        } else {
+            alert('Vælg venligst en Pokémon først!');
+        }
+    });
